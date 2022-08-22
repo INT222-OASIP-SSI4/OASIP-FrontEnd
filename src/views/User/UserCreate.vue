1a1f61c3e2c2fcbf9e0fa6f30f505985dbafbcf3
smile1346 <kantapon.tantra@mail.kmutt.ac.th>
2022-08-23 04:07:57 +0700

    [UPDATE] validate duplicate create user

--- src/views/User/UserCreate.vue
+++ src/views/User/UserCreate.vue
@@ -1,45 +1,53 @@
 <script setup>
-import UserCreateForm from '../../components/User/UserCreateForm.vue';
-import {ref} from 'vue';
-import { useRoute, useRouter } from 'vue-router';
-import { onBeforeMount } from '@vue/runtime-core';
+import UserCreateForm from '../../components/User/UserCreateForm.vue'
+import { ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { onBeforeMount } from '@vue/runtime-core'
 
-const route = useRoute();
-const router = useRouter();
-const users = ref([]);
+const route = useRoute()
+const router = useRouter()
+const users = ref([])
 
 //get all users
 const getUsers = async () => {
-  const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/users`);
+  const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/users`)
   if (res.status === 200) {
-    let data = await res.json();
-    users.value = data;
+    let data = await res.json()
+    users.value = data
   } else {
-    console.log("error, cannot get data");
-  }
-};
-
-//create new user
-const createUser = async (newUser) => {
-  const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/users`, {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify(newUser),
-  })
-  if (res.status === 201) {
-    let data = await res.json();
-    alert('Created user successfully');
-    router.push({ name: 'userDetail', query: {id: data.id}});
+    console.log('error, cannot get data')
   }
 }
 
+//create new user
+const createUser = async (newUser) => {
+  if (users.value.map(() => (newUser.userName = users.value.userName))) {
+    alert('userName is duplicate, Please input again')
+  } else {
+    if (users.value.map(() => (newUser.userEmail = users.value.userEmail))) {
+      {
+        alert('userEmail is duplicate, Please input again')
+      }
+    } else {
+      const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/users`, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify(newUser),
+      })
+      if (res.status === 201) {
+        let data = await res.json()
+        alert('Created user successfully')
+        router.push({ name: 'userDetail', query: { id: data.id } })
+      }
+    }
+  }
+}
 
 onBeforeMount(async () => {
-  await getUsers();
-});
-
+  await getUsers()
+})
 </script>
 
 <template>
