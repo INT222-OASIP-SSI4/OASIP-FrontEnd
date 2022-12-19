9258d5a6c10deef84f1e559d626bfa946fe948f7
Jajahz <jajah.asmp@gmail.com>
2022-12-19 19:31:37 +0700

    [Update] update api service

--- src/composables/ApiService.js
+++ src/composables/ApiService.js
@@ -3,13 +3,13 @@ import { ref } from 'vue'
 
 const accessToken = ref(localStorage.getItem('accessToken'))
 
-const configGet = {
+const configAuth = {
   headers: {
     Authorization: `Bearer ${accessToken.value}`,
   },
 }
 
-const configAuth = {
+const configPostPut = {
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${accessToken.value}`,
@@ -17,30 +17,34 @@ const configAuth = {
 }
 
 const configCreateEvent = {
-    headers: {
-        'Content-Type': 'multipart/form-data'
-    }
+  headers: {
+    'Content-Type': 'multipart/form-data',
+  },
 }
 
 class ApiService {
   // Categories Service
   async getCategories() {
-    return await api.get(`/api/eventcategories`, configGet)
+    return await api.get(`/api/eventcategories`, configAuth)
   }
   async getCategoryById(id) {
-    return await api.get(`/api/eventcategories/${id}`, configGet)
+    return await api.get(`/api/eventcategories/${id}`, configAuth)
   }
   async editCategory(id, body) {
-    return await api.put(`/api/eventcategories/${id}`, body, configAuth)
+    return await api.put(`/api/eventcategories/${id}`, body, configPostPut)
   }
-  // Events
+  // Events Service
   async getEvents() {
-    return await api.get(
-      `/api/events/validate`
-    )
+    return await api.get(`/api/events/validate`)
   }
-  async createEvent(body){
-    return await api.post(`/api/events`, body, configCreateEvent)
+  async getEventById(id) {
+    return await api.get(`/api/events/${id}`, configAuth)
+  }
+  // async createEvent(body) {
+  //   return await api.post(`/api/events`, body, configCreateEvent)
+  // }
+  async deleteEvent(id){
+    return await api.delete(`/api/events/${id}`, configAuth)
   }
 }
 export default new ApiService()
